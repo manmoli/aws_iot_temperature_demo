a4bfb1983c64078df7c247f466d55672cb3124a2
Manuel Molina <manuelmolinapena@gmail.com>
2024-05-16 13:14:02 -0600

    [FIX]: Remove promise and get data directly from event.

--- lambda/lambda.js
+++ lambda/lambda.js
@@ -2,28 +2,19 @@ const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-  console.log('Received IoT data: ', JSON.stringify(event));
+  try {
+  console.log('Received IoT data: ', event);
   
-  const promises = event.Records.map((record) => {
-    const buffer = Buffer.from(record.kinesis.data, 'base64').toString('utf-8');
-    console.log('Buffer data: ', buffer)
-    const payload = JSON.parse(buffer);
-    
     const params = {
       TableName: 'TemperatureData',
       Item: {
-        deviceId: payload.deviceId,
+        deviceId: event.deviceId,
         timestamp: new Date().getTime(),
-        temperature: payload.temperature
+        temperature: event.temperature
       }
     };
-    
+        
     return dynamoDB.put(params).promise();
-    
-  })
-  
-  try {
-    Promise.all(promises);
   } catch (error) {
     console.error('Error storing data in DynamoDB: ', error);
     throw error;
