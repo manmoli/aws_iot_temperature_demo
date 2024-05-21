84be05c945a0e997623f12ec09568fa43ac71e0e
Manuel Molina <manuelmolinapena@gmail.com>
2024-05-20 19:16:24 -0600

    [FEAT]: Add sns publish.

--- lambda/lambda.js
+++ lambda/lambda.js
@@ -1,10 +1,31 @@
 const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
+const sns = new AWS.SNS();
+
+const publishToSNS = async (message) => {
+  const params = {
+    Message: message,
+    TopicArn: '<Your-SNS-Topic-ARN>'
+  };
+
+  try {
+    await sns.publish(params).promise();
+    console.log('Message published to SNS');
+  } catch (err) {
+    console.error('Error publishing to SNS:', err);
+  }
+};
+
+
 exports.handler = async (event) => {
   try {
-  console.log('Received IoT data: ', event);
-  
+    console.log('Received IoT data: ', event);
+
+    if (event.temperature > 27) {
+      publishToSNS(`temperature is higher than normal: ${27}`);
+    }
+
     const params = {
       TableName: 'TemperatureData',
       Item: {
@@ -13,7 +34,7 @@ exports.handler = async (event) => {
         temperature: event.temperature
       }
     };
-        
+
     return dynamoDB.put(params).promise();
   } catch (error) {
     console.error('Error storing data in DynamoDB: ', error);
